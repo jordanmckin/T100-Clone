91f45ebced859ce3562459363c036ea2c0b26643
Jordan McKinney <jmck486@wgu.edu>
2024-05-15 12:19:25 -0700

    Playing around with html and javascript to make a tis-100 clone V1

---
+++ backend.js
@@ -0,0 +1,13 @@
+
+
+
+
+//special search for label, <>, ! & comments
+
+const valid_instructions = new Set("!", "#", ":", "<>", "MOV", "NOP", "SWP", "SAV", "ADD", "SUB", "NEG", "JMP", "JEZ", "JNZ", "JGZ", "JLZ", "JRO");
+
+//make PORT connections that are LEVEL specific
+
+//make a function that connects the nodes that is searchable during runtime
+
+//make a function that runs all the nodes
